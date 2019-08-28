f807020b4eefe282a7dd02cc188b6093586598f1
황성민 <>
2019-08-28 20:15:22 +0900

    no message

--- Classes/common/Define.h
+++ Classes/common/Define.h
@@ -118,8 +118,7 @@ static const std::string FONT_ROBOTO_BLACK          = DIR_FONT + "Roboto-Black.t
 static const std::string DIR_SOUND                  = "sounds/";
 
 static const std::string SOUND_BGM_MAIN             = DIR_SOUND + "bgm_01.mp3";
-static const std::string SOUND_BGM_GAME             = DIR_SOUND + "bgm_03.mp3";
-static const std::string SOUND_BGM_COMING_SOON      = DIR_SOUND + "bgm_03.mp3";
+static const std::string SOUND_BGM_GAME             = DIR_SOUND + "bgm_02.mp3";
 
 static const std::string SOUND_BUTTON_CLICK         = DIR_SOUND + "effect_01.mp3";
 static const std::string SOUND_TILE_DROP            = DIR_SOUND + "effect_08.mp3";
